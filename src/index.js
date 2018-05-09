823ac2a5844e837c34e05032dd8af5503de79217
Nitish Mallick <nitish@junati.com>
2018-05-09 22:59:11 +0530

    Added class based App component

--- src/index.js
+++ src/index.js
@@ -1,18 +1,24 @@
-import React from 'react';
+import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
-import jsonloader from 'jsonloader';
-
 import SearchBar from './components/search_bar';
+const API_KEY = 'AIzaSyDj8utu0QZ6G8V64_Ft6wBduHy5kLsRJoU';
 
-const config =  new jsonloader('./../config.json');
+class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {videos: []};
+    YTSearch({key: API_KEY, term: 'Iron Man'}, (videos) => {
+      this.setState({videos});
+    });
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+      </div>
+    );
+  }
 
-YTSearch({key: 'AIzaSyDj8utu0QZ6G8V64_Ft6wBduHy5kLsRJoU', term: 'Iron Man'}, function(data){
-  console.log(data);
-});
-const App = () => (<div>
-    <SearchBar />
-  </div>);
-
-
+}
 ReactDOM.render(<App/>, document.querySelector('.container'));
