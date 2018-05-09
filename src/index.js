9ca2b528fb0560ea12ac10b40b7702d1b057b820
insphere-nitish <insphere.nitish@gmail.com>
2018-05-09 18:58:37 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -1,8 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import jsonloader from 'jsonloader';
 
 import SearchBar from './components/search_bar';
 
+const config =  new jsonloader('./../config.json');
+
+YTSearch({key: 'AIzaSyDj8utu0QZ6G8V64_Ft6wBduHy5kLsRJoU', term: 'Iron Man'}, function(data){
+  console.log(data);
+});
 const App = () => (<div>
     <SearchBar />
   </div>);
