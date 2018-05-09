65ed4bc7c8ddc1587cf26b0edcabcb557909341a
Nitish Mallick <nitish@junati.com>
2018-05-09 23:24:01 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -2,13 +2,14 @@ import React, {Component} from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 const API_KEY = 'AIzaSyDj8utu0QZ6G8V64_Ft6wBduHy5kLsRJoU';
 
 class App extends Component {
   constructor(props){
     super(props);
     this.state = {videos: []};
-    YTSearch({key: API_KEY, term: 'Iron Man'}, (videos) => {
+    YTSearch({key: API_KEY, term: 'Iron Man'}, (videos) =>  {
       this.setState({videos});
     });
   }
@@ -16,6 +17,7 @@ class App extends Component {
     return (
       <div>
         <SearchBar />
+        <VideoList videos={this.state.videos} />
       </div>
     );
   }
