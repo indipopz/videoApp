d992ac20c1d6be1edd8f17e359411e2df4880286
insphere-nitish <insphere.nitish@gmail.com>
2018-05-10 13:43:37 +0530

    Added interface and iframe

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -2,12 +2,17 @@ import React from 'react';
 import VideoListItem from './video_list_item';
 
 const VideoList = (props) => {
-  const VideoItems = props.videos.map((video) => {
-    return <VideoListItem video={vodeo} />;
+  const videoItems = props.videos.map((video) => {
+    return (
+      <VideoListItem
+        onVideoSelect ={props.onVideoSelect}
+        key={video.etag}
+        video={video} />
+    );
   });
   return(
     <ul className="col-md-4 list-group">
-    {VideoItems}
+      {videoItems}
     </ul>
   );
 };
--- src/index.js
+++ src/index.js
@@ -3,21 +3,31 @@ import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 const API_KEY = 'AIzaSyDj8utu0QZ6G8V64_Ft6wBduHy5kLsRJoU';
 
 class App extends Component {
   constructor(props){
     super(props);
-    this.state = {videos: []};
+    this.state = {
+      videos: [],
+      selectedVideo: null
+    };
     YTSearch({key: API_KEY, term: 'Iron Man'}, (videos) =>  {
-      this.setState({videos});
+      this.setState({
+        videos:videos,
+        selectedVideo: videos[0]
+      });
     });
   }
   render(){
     return (
       <div>
         <SearchBar />
-        <VideoList videos={this.state.videos} />
+        <VideoDetail video={this.state.selectedVideo} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({selectedVideo})}
+          videos={this.state.videos} />
       </div>
     );
   }
