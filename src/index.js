748579a59764596fd78d8bbda664896ed0f4ffb7
insphere-nitish <insphere.nitish@gmail.com>
2020-02-14 18:40:34 +0530

    updates

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
 import VideoList from './components/video_list';
 import VideoDetail from './components/video_detail';
-const API_KEY = 'AIzaSyDj8utu0QZ6G8V64_Ft6wBduHy5kLsRJoU';
+const API_KEY = 'XXXXX';
 
 class App extends Component {
   constructor(props){
